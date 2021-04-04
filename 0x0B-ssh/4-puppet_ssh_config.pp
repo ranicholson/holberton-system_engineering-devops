63691a925676d9ca3fb77f1e78a07794b2059844
ranicholson <yranicho2@gmail.com>
2021-04-04 18:27:28 +0000

    Task 3

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# using puppet to adjust ssh configuration
+
+exec { 'ssh_config':
+  path    => '/bin',
+  command => 'echo "PasswordAuthentication no" >> /etc/ssh/ssh_config; echo "IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+}
