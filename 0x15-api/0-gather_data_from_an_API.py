9eb02f5017fcb9b813b8f5c3e4a239a5f3331f0b
ranicholson <ranicho2@gmail.com>
2021-05-03 18:20:16 +0000

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script to use REST API to gather information"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url_target = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url_target + "users/{}".format(sys.argv[1])).json()
+    tasks = requests.get(url_target + "todos", params={
+                        "userId": sys.argv[1]}).json()
+
+    completetasks = []
+    for todos in tasks:
+        if todos.get("completed") is True:
+            completetasks.append(todos.get("title"))
+
+    print("Employee {} is done with tasks({}/{}:".format(
+            employee.get("name") en(completetasks), len(tasks)))
+    for title in completetasks:
+        print("\t {}".format(title))
