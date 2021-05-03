361b9bc301bb3f6e0fea682c4700788a8c58c15f
ranicholson <ranicho2@gmail.com>
2021-05-03 21:26:20 +0000

    Task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Script to use REST API to gather information"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url_target = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url_target + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        for user in users:
+            ulist = []
+            USER_ID = user.get("id")
+            for task in requests.get(url_target + "todos",
+                                     params={"userId": USER_ID}).json():
+                ulist.append({"username": user.get("username"),
+                             "task": task.get("title"),
+                              "completed": task.get("completed")})
+            jdict = {USER_ID: ulist}
+            json.dump(jdict, jsonfile)
